fe93f53b027b0179e8ae9e354be257f68ad59b28
mintoua <toupkandi.mintoua@esprit.tn>
2023-07-26 20:52:00 +0100

    third commit changed welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Bienvenu chez LOGONE DIGITALE");
+        System.out.println("Ceci est juste un projet sur la branche master" +
+                "pour le test de création d'un simple projet JAVA");
     }
 }
