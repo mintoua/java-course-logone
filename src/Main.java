d0db66b4291796e43cd0e9d46696b542f86c0055
mintoua <toupkandi.mintoua@esprit.tn>
2023-08-11 15:34:16 +0100

    Classe, Object, Instance, Méthodes, Attributs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,39 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Bienvenu chez LOGONE DIGITALE");
-        System.out.println("Ceci est juste un projet sur la branche master" +
-                "pour le test de création d'un simple projet JAVA");
+        System.out.println("Ceci est juste un projet sur la branche seance_5");
+
+
+
+        //Instances de la classe Voiture
+ /*       Voiture voiture1 = new Voiture();
+        Voiture voiture2 = new Voiture("blue","mercedez",50000);*/
+
+
+        Utilisateur user1 = new Utilisateur();
+
+
+        Utilisateur user2 =
+                new Utilisateur(
+                "mta",
+                "tpkd",
+                "mta@gmail.com",
+                45);
+
+
+        Utilisateur user3 =
+                new Utilisateur(
+                "nankap",
+                "serge");
+
+        Voiture v1 = new Voiture();
+        Voiture v2 = new Voiture(
+                "rouge",
+                "toyota",
+                20000);
+
+        v2.avancer(20);
+        v2.reculer();
     }
 }
---
+++ src/Voiture.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Voiture {
+
+    //Attributs
+    String couleur;
+    String marque;
+    double prix;
+
+
+    //Construteurs
+    Voiture()
+    {
+
+    }
+
+    Voiture(String color, String marq, double price)
+    {
+        this.couleur = color;
+        this.marque = marq;
+        this.prix = price;
+    }
+
+
+    //Méthodes
+    //syntaxe de création d'une méthode
+    // type_de_retour nom_de_la_methode( paramètres ) { }
+    // void
+
+    String avancer(double vitesse){
+        return "avance avec une vitessse" + vitesse;
+    }
+
+    void reculer()
+    {
+        System.out.println("reculer");
+    }
+
+}
