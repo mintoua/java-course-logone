40e4e7764f484bc599c1d22a7cf85a5c7381e76e
mintoua <toupkandi.mintoua@esprit.tn>
2023-09-04 20:36:00 +0100

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bienvenu chez LOGONE DIGITALE");
-        System.out.println("Ceci est juste un projet sur la branche master" +
-                "pour le test de création d'un simple projet JAVA");
+        System.out.println("Ceci est juste un projet sur la branche correction gestionEquipe");
     }
 }
